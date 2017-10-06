27f9b0b20a49226ee9290aaa2c54bb194306752a
Nikolai Pawlowski <nikolaip@gmail.com>
2017-10-06 09:49:51 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,7 +28,10 @@ def monopoly_with_third_tier
 			pieces: 4,
 			names: {},
 			rent_in_dollars: {
-				one_piece_owned: 25
+				one_piece_owned: 25,
+				two_piece_owned: 50,
+				three_piece_owned: 75,
+				four_piece_owned: 100
 			}
 		}
 	}
